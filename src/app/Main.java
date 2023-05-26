2ed012be072aaf48d7f83501943cafd3a90ed51f
OleksandrChaika13 <axelrod1347@gmail.com>
2023-05-26 10:19:31 +0300

    Add miles to kms convertation

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My amazing app for convertation.");
+        double miles = 15;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+    }
+
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
     }
 }
